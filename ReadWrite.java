7d8b8c26c7a882e8c847a18e13796495ce0f9b9e
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-05-18 17:45:54 +0200

    ReadWrite

---
+++ ReadWrite.java
@@ -0,0 +1,29 @@
+
+/*
+ * In this challenge, 
+ * you must read  integers from stdin and then print them to stdout. 
+ * Each integer must be printed on a new line. To make the problem a little easier, 
+ * a portion of the code is provided for you in the editor below.
+ */
+import java.util.*;
+
+public class ReadWrite {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+        // Complete this line
+
+        System.out.println(a);
+
+        // Complete this line
+        System.out.println(b);
+
+        // Complete this line
+        System.out.println(c);
+    }
+}
